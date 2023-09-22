f2f3e41c63772c345ac3ca2592ee803dc20bbddc
Igor-QAP137 <igor.testing1988@gmail.com>
2023-09-22 10:09:14 +0300

    A new file is added

---
+++ 18.8.19.py
@@ -0,0 +1,23 @@
+total_cost = 0
+tickets = int(input("Введите колличество билетов: "))
+for age in range(tickets):
+    age = int(input("Введите возвраст посетителя: "))
+    if age < 18:
+        total_cost  += 0
+    elif 18<= age <= 25:
+        total_cost  += 990
+    elif age > 25:
+        total_cost  += 1390
+if tickets == 0:
+    print("Проходят только дети!",)
+else:
+    print("Колличество билетов: ", tickets)
+if tickets > 3:
+    discount = total_cost / 100 * 10
+    print("Скидка составляет:", "%.2f" % discount, "руб.")
+    print("К оплате с учетом скидки:", "%.2f" % (total_cost - discount), "руб.")
+if tickets < 4:
+    Nodiscount = total_cost
+    print("К оплате:", "%.2f" % Nodiscount, "руб.")
+
+
